c897e8405667746a81d1c01b69a630b42b6d177f
Hugo Novo Gouvea <97973663+Hugo-Novo-Gouvea@users.noreply.github.com>
2026-01-06 23:52:28 -0300

    Aulas 0 e 1

---
+++ Aula_0/Program.cs
@@ -0,0 +1,39 @@
+﻿string nomeDoAluno;
+string nomeDoCurso;
+int idade;
+decimal preco;
+
+// 1. Coletando Nome
+Console.Write("Informe o nome do aluno: ");
+nomeDoAluno = Console.ReadLine() ?? "";
+
+// 2. Coletando Idade
+Console.Write("Informe a idade do aluno: ");
+while (!int.TryParse(Console.ReadLine(), out idade) || idade < 0)
+{
+    Console.WriteLine("Valor Não permitido... Tente novamente");
+    Console.Write("Informe a idade do aluno: ");
+}
+
+// 3. Coletando Curso
+Console.Write("Informe o nome do curso: ");
+nomeDoCurso = Console.ReadLine() ?? "";
+
+// 4. Coletando Preço
+Console.Write("Informe o preço do curso: ");
+while(!decimal.TryParse(Console.ReadLine(), out preco) || preco < 0)
+{
+    Console.WriteLine("Valor Não permitido... Tente novamente");
+    Console.Write("Informe o preço do curso: ");
+}
+
+Console.Clear();
+
+Console.WriteLine("=================================");
+Console.WriteLine("       RELATÓRIO DE MATRÍCULA    ");
+Console.WriteLine("=================================");
+Console.WriteLine($"Aluno: {nomeDoAluno}");
+Console.WriteLine($"Idade: {idade} anos");
+Console.WriteLine($"Curso: {nomeDoCurso}");
+Console.WriteLine($"Investimento: {preco:C}"); // O :C formata como dinheiro
+Console.WriteLine("=================================");
---
+++ Aula_1/Program.cs
@@ -0,0 +1,60 @@
+﻿string nome;
+int opcao;
+
+Console.Write("Bem vindo ao Banco! Por favor, informe seu nome: ");
+nome = Console.ReadLine() ?? "";
+
+do 
+{
+    Console.Clear();
+    
+    Console.WriteLine("======================================");
+    Console.WriteLine("======= AUTO-ATENDIMENTO BANCO =======");
+    Console.WriteLine("======================================");
+    Console.WriteLine("=                                    =");
+    Console.WriteLine("=        1 - Contratar Plano         =");
+    Console.WriteLine("=        2 - Suporte Técnico         =");
+    Console.WriteLine("=        3 - Financeiro              =");
+    Console.WriteLine("=        4 - Sair                    =");
+    Console.WriteLine("=                                    =");
+    Console.WriteLine("======================================");
+    Console.WriteLine("");
+    Console.Write("Informe a opção desejada: ");
+
+    while(!int.TryParse(Console.ReadLine(), out opcao) || opcao < 0)
+    {
+        Console.WriteLine("Opção Invalida... Tente Novamente");
+        Console.Write("Informe a opção desejada: ");
+    }
+
+    switch(opcao)
+    {
+        case 1:
+            Console.WriteLine("Redirecionando para setor de vendas...");
+            break;
+        case 2:
+            Console.WriteLine("Enviando e-mail para suporte@dev.com...");
+            break;
+        case 3:
+            Console.WriteLine("Consultando faturas em aberto...");
+            break;
+        case 4:
+            Console.WriteLine($"Encerrando aplicação. Tchau! [{nome}]");
+            break; 
+        default:
+            Console.WriteLine("Opção não disponivel no momento");
+            break;
+    }
+
+    if (opcao != 4)
+    {
+        Console.WriteLine("\nPressione qualquer tecla para voltar ao menu...");
+        Console.ReadKey();
+    }
+}
+while (opcao != 4);
+
+
+
+
+
